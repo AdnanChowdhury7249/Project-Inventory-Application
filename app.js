c110c491de05263cdbb040d8bccba832375e97ff
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-02-26 18:55:09 +0000

    initialised react for the frontend

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+// });
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'React is connected to Express!' });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
