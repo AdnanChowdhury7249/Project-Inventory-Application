14f12c8c2cb57cc5b2eebcfe799e129f74b84d6c
AdnanChowdhury7249 <adnanchowdhury7249@gmail.com>
2025-03-19 13:16:51 +0000

    buttons for navigation and button styling

--- frontend/src/pages/AllItemsPage.jsx
+++ frontend/src/pages/AllItemsPage.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { getAllItems } from "../api";
 import ItemCard from "../components/ItemCard";
+import { useNavigate } from "react-router-dom";
 
 const AllItemsPage = () => {
+  const navigate = useNavigate();
   const [items, setItems] = useState([]);
   const [error, setError] = useState(null);
 
@@ -22,6 +24,24 @@ const AllItemsPage = () => {
   return (
     <div>
       <h2 className="text-center text-2xl font-bold py-10">All Items</h2>
+      <div className="flex justify-center my-10">
+        <div className="flex bg-gray-100 rounded-b-md p-1 shadow-md w-1/2 max-w-lg justify-between">
+          <button
+            className="flex-1 px-6 py-2 text-gray-600 font-medium rounded-md  transition duration-300 
+                 hover:bg-white hover:shadow-md focus:bg-white focus:shadow-md cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            Back to categories
+          </button>
+          <button
+            className="flex-1 px-6 py- text-gray-600 font-medium rounded-md  transition duration-300 
+                 hover:bg-white hover:shadow-md focus:bg-white focus:shadow-md cursor-pointer"
+            onClick={() => navigate("/create-category")}
+          >
+            Create Category
+          </button>
+        </div>
+      </div>
       <div className="flex justify-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10 w-full max-w-5xl">
           {items.length > 0 ? (
@@ -36,3 +56,4 @@ const AllItemsPage = () => {
 };
 
 export default AllItemsPage;
+
--- frontend/src/pages/ItemsPage.jsx
+++ frontend/src/pages/ItemsPage.jsx
@@ -9,6 +9,24 @@ const ItemPage = () => {
       <button
         className="block mx-auto text-x2 font-bold py-2 px-4 border rounded-sm cursor-pointer mb-4"
         onClick={() => navigate(`/category/${id}/add-item`)}>add Item</button>
+      <div className="flex justify-center my-10">
+        <div className="flex bg-gray-100 rounded-b-md p-1 shadow-md w-1/2 max-w-lg justify-between">
+          <button
+            className="flex-1 px-6 py-2 text-gray-600 font-medium rounded-md  transition duration-300 
+                 hover:bg-white hover:shadow-md focus:bg-white focus:shadow-md cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            Back to categories
+          </button>
+          <button
+            className="flex-1 px-6 py- text-gray-600 font-medium rounded-md  transition duration-300 
+                 hover:bg-white hover:shadow-md focus:bg-white focus:shadow-md cursor-pointer"
+            onClick={() => navigate("/items")}
+          >
+            All Items
+          </button>
+        </div>
+      </div>
       <ItemList />
 
     </div>
